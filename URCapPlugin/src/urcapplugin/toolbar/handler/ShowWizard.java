e2eba2cca91da997de1b6738c5f10adc73f5af86
Unknown <jacos16@student.sdu.dk>
2019-03-04 15:17:31 +0100

    Setup toolbar so dropdown has different handlers

--- URCapPlugin/src/urcapplugin/toolbar/handler/ShowWizard.java
+++ URCapPlugin/src/urcapplugin/toolbar/handler/ShowWizard.java
@@ -3,23 +3,27 @@ package urcapplugin.toolbar.handler;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.common.NotDefinedException;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.osgi.framework.BundleContext;
+import testplugin.*;
 
 import urcaplugin.wizard.URCapWizard;
 
 /**
  * Class to start up the wizard when toolbar icon is clicked.
+ * 
  * @author jacob
  *
  */
-public class ShowWizard extends AbstractHandler{
+public class ShowWizard extends AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		
+		TestPluginClass sss = new TestPluginClass();
+		sss.helloworld();
 		URCapWizard wizard = new URCapWizard();
-		WizardDialog dialog = new WizardDialog(HandlerUtil
-				.getActiveShell(event), wizard);
+		WizardDialog dialog = new WizardDialog(HandlerUtil.getActiveShell(event), wizard);	
 		dialog.open();
 		return null;
 	}
