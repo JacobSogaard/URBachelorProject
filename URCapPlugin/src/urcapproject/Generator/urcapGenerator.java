c855a1e6ee24771c495aba50dc37b823975d479f
Tanya <tanja331@live.dk>
2019-03-11 10:29:30 +0100

    possible to create new project from code...

--- URCapPlugin/src/urcapproject/Generator/urcapGenerator.java
+++ URCapPlugin/src/urcapproject/Generator/urcapGenerator.java
@@ -1,8 +1,8 @@
 package urcapproject.Generator;
 
-
 import java.io.File;
 import java.util.Collections;
+import java.util.Properties;
 
 import org.apache.maven.shared.invoker.DefaultInvocationRequest;
 import org.apache.maven.shared.invoker.DefaultInvoker;
@@ -13,28 +13,32 @@ import org.apache.maven.shared.invoker.MavenInvocationException;
 
 public class urcapGenerator {
 
-
+	private NewProjectModel model;
 
 	public urcapGenerator() {
+
+		this.model = new NewProjectModel();
 	}
 
-	
 	public void executeMavenCommand() {
+
 		InvocationRequest request = new DefaultInvocationRequest();
-		request.setPomFile(new File("C:\\Users\\Bruger\\Documents\\URBachelorProject\\URCapPlugin\\pom.xml"));
-		request.setGoals(Collections.singletonList("install"));
+		//request.setPomFile(new File("C:\\Users\\Bruger\\Documents\\URBachelorProject\\URCapPlugin\\pom.xml"));
+		request.setBaseDirectory(new File("C:\\Users\\Bruger\\Documents\\Empty"));
+		request.setGoals(Collections.singletonList("archetype:generate"));
+		request.setBatchMode(true);
+		request.setProperties(this.model.setDefaultProperties());
 
 		Invoker invoker = new DefaultInvoker();
 		invoker.setMavenHome(new File(System.getenv("MAVEN_HOME")));
+
 		try {
 			InvocationResult result = invoker.execute(request);
 		} catch (MavenInvocationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 	}
-	
-	public void setProperties() {
-		
-	}
+
 }
